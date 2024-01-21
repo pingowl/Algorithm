89f30c16229463669f27eb4780f6e8e29ba30a0b
Bomin Kwon <101239440+pingowl@users.noreply.github.com>
2024-01-21 22:52:25 +0900

    [Silver IV] Title: 주몽, Time: 80 ms, Memory: 2080 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+int n, m, a[15000], cnt;
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin >> n >> m;
+    for(int i=0; i<n; i++) cin >> a[i];
+    sort(a, a+n);
+    for (int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(a[i]+a[j]==m) cnt++;
+        }
+    }
+    cout << cnt << "\n";
+    return 0;
+}
