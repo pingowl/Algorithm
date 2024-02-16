e41e56cb89f2d6f7e7812eccadd8c306cab233d7
Bomin Kwon <101239440+pingowl@users.noreply.github.com>
2024-02-16 22:17:57 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll; 
+ll n,atk,t,a,h,H,mx,damage;
+
+int main(){
+   ios_base::sync_with_stdio(0);cin.tie(0);
+   cin>> n >> atk; 
+   for(int i = 0; i < n; i++){
+      cin>> t >> a >> h;
+      if(t == 1){ 
+         damage = a * (ceil((double)h/atk)-1);  
+         if(H < damage){
+            mx += damage - H;
+            H = 0; 
+         }else H -= damage;
+      }else{
+         atk += a;
+         H = min(H+h,mx);
+      }
+   }
+   cout << mx + 1 << "\n";
+} 
