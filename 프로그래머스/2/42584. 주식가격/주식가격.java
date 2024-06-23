5b3e590e8bb89b0900fd9b3338c2333868a9fc15
Bomin Kwon <101239440+pingowl@users.noreply.github.com>
2024-06-23 16:42:23 +0900

    [level 2] Title: 주식가격, Time: 11.89 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int n = prices.length;
+        int[] answer = new int[n];
+        
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                answer[i]++;
+                if(prices[i] > prices[j]){
+                    break;
+                }
+            }
+        }
+        
+        answer[n-1]=0;
+        return answer;
+    }
+}
